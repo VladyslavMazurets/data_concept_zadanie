ec6a5b3de49aae9de711d665c42ddfcdc64ef011
VladyslavMazurets <vladmazurec@gmail.com>
2023-04-07 20:07:26 +0200

    final version

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,16 @@ import './App.scss';
 import Weblayer from "./Weblayer/Weblayer";
 
 function App() {
-
   const [btnClick, setBtnClick] = useState(false);
 
   return (
     <>
      <div className="home">
-          <button type="button" className="home__btn"
+            <button type="button" className="home__btn"
           onClick={() => setBtnClick(prevState => !prevState)}>
             Click on me
           </button>
-
-          {btnClick && <Weblayer setBtnClick={setBtnClick}/>}
+          {btnClick && <Weblayer setBtnClick={setBtnClick} />}
      </div>
     </>
   );
--- src/Weblayer/Weblayer.jsx
+++ src/Weblayer/Weblayer.jsx
@@ -4,43 +4,68 @@ import './Weblayer.scss';
 
 function Weblayer({ setBtnClick }) {
     const [submitState, setSubmitState] = useState(false);
+    const [close, setClose] = useState(false);
     const name = useRef('');
     const mail = useRef('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        console.log("Name: ", name.current.value);
-        console.log( "E-mail: ", mail.current.value )
+        const isEmail =  
+        /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(mail.current?.value);
 
+        if(!isEmail){
+             alert('Wrong email address!');
+             return;
+        }
+
+        console.log("Name:", name.current.value);
+        console.log( "E-mail:", mail.current.value )
         setSubmitState(true)
 
+        setTimeout(() => {
+            setClose(true)
+        }, 800)
+        
         setTimeout(() => {
             setBtnClick(false)
         }, 1500);
     }
 
+    const handleClose = () => {
+        setClose(true)
+
+        setTimeout(() => {
+            setBtnClick(false)
+        }, 1500)
+    }
+
   return (
     <>
-        <div className='weblayer'>
+        <div className={`weblayer ${ !close ? 'active' : 'passive'}`}>
+
+            <a type='button' className='weblayer__closebtn'
+            onClick={handleClose}/>
+
             <div className='weblayer__container'>
                 <div className='weblayer__header'>
                     Hľadáme práve teba
                 </div>
-
                 <span className='weblayer__subheader'>
                     Discover the solutions for industry and get a 
                     tailored offer now.
                 </span>
-
              {!submitState ?
                 <form onSubmit={handleSubmit} className='weblayer__form'>
-                    <input id='name' type='text' placeholder='Your name' 
-                    ref={name}
-                    required/>
-                    <input id='email' type='email' placeholder='Your e-mail' 
-                    ref={mail}
-                     required/>
+
+                    <input name='name' type='text' placeholder='Your name' 
+                    ref={name} required/>
+
+                    <input name='email' type='email' placeholder='Your e-mail' 
+                    ref={mail} required 
+                    onChange={() => mail.current.value.replace
+                    (/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, '')}/>
+
                     <button type='submit'> 
                         Experience now 
                     </button>
